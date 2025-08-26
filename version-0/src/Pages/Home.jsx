69dd0421093e9a62c5681eb6a7389fb8f94b94ef
Joyfull-Happiness <lajoie.bradley2@gmail.com>
2025-08-26 01:37:19 -0700

    updated home page

--- version-0/src/Pages/Home.jsx
+++ version-0/src/Pages/Home.jsx
@@ -2,19 +2,13 @@ import data from "../localData.js";
 import CountryDetail from "../compone./CountryDetail.jsx";
 import React, { useState } from "react";
 
-export default Home function{
-
-    return(
-        <>
-        <headder>
-            <h3> Where in the World?</h3>
-            <a hef>Saved Countries</a>
-
-
-        </headder>
-        </>
-
-
-
-    )
+export default function Home({}) {
+  return (
+    <>
+      <headder>
+        <h3> Where in the World?</h3>
+        <a hef>Saved Countries</a>
+      </headder>
+    </>
+  );
 }
