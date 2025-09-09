3b107de139d83842b0329da0a6881cd9f3ef5808
Joyfull-Happiness <lajoie.bradley2@gmail.com>
2025-09-09 08:39:11 -0700

    updated page

--- version-0/src/Pages/Home.jsx
+++ version-0/src/Pages/Home.jsx
@@ -1,16 +1,33 @@
 import localData from "../../localData.js";
 import CountryDetail from "./CountryDetail.jsx";
+import CountryCard from "./CountryCard.jsx";
+// import SavedCountries from "./SavedCountries.jsx";
 import React, { useState } from "react";
 
 export default function Home({}) {
+  const sortedCountries = [...localData].sort((a, b) =>
+    a.name.common.localeCompare(b.name.common)
+  );
   return (
-    <header className="header">
-      <a href="/home" className="homeHeader-link">
-        Where in the world?
-      </a>
-      <a href="/saved" className="savedCountries-link">
-        Saved Countries
-      </a>
-    </header>
+    <>
+      <header className="header">
+        <a href="/Home" className="homeHeader-link">
+          Where in the world?
+        </a>
+        <a href="/SavedCountries" className="savedCountries-link">
+          Saved Countries
+        </a>
+      </header>
+      <div className="card-container">
+        <div>
+          {sortedCountries.map((country) => (
+            <CountryCard
+              key={country.cca3 || country.cca2 || country.name?.common}
+              country={country}
+            />
+          ))}
+        </div>
+      </div>
+    </>
   );
 }
